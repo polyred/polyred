eaf463ac4e5737036c0e73d48173c4f5b204aeb2
Changkun Ou <hi@changkun.de>
2021-06-30 23:52:13 +0200

    vendor: add gui related wrappers

--- examples/polywine/main.go
+++ examples/polywine/main.go
@@ -22,7 +22,7 @@ import (
 
 func main() { mainthread.Init(fn) }
 func fn() {
-	width, height := 400, 400
+	width, height := 500, 500
 	w := gui.NewWindow(
 		gui.WithTitle(fmt.Sprintf("polyred - %dx%d", width, height)),
 		gui.WithSize(width, height),
@@ -47,7 +47,7 @@ func fn() {
 		ProjectionMatrix: cam.ProjMatrix(),
 	}
 	buf := render.NewBuffer(image.Rect(0, 0, width, height))
-	idx, tri := geo(1000)
+	idx, tri := geo(10000)
 
 	w.MainLoop(func() *image.RGBA {
 		buf.Clear()
@@ -77,19 +77,19 @@ func geo(num int) ([]uint64, []*primitive.Vertex) {
 		idx[i+1] = i + 1
 		idx[i+2] = i + 2
 		tri[i] = &primitive.Vertex{
-			Pos: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1),
+			Pos: math.NewVec4((rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, 1),
 			UV:  math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, 0, 1),
 			Nor: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1).Unit(),
 			Col: color.RGBA{uint8(rand.Int()), uint8(rand.Int()), uint8(rand.Int()), 255},
 		}
 		tri[i+1] = &primitive.Vertex{
-			Pos: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1),
+			Pos: math.NewVec4((rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, 1),
 			UV:  math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, 0, 1),
 			Nor: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1).Unit(),
 			Col: color.RGBA{uint8(rand.Int()), uint8(rand.Int()), uint8(rand.Int()), 255},
 		}
 		tri[i+2] = &primitive.Vertex{
-			Pos: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1),
+			Pos: math.NewVec4((rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, (rand.Float64()*2-1)/10, 1),
 			UV:  math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, 0, 1),
 			Nor: math.NewVec4(rand.Float64()*2-1, rand.Float64()*2-1, rand.Float64()*2-1, 1).Unit(),
 			Col: color.RGBA{uint8(rand.Int()), uint8(rand.Int()), uint8(rand.Int()), 255},
