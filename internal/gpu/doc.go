ced5fc54f7e7a52b838f416c62d225f3c9077410
Changkun Ou <hi@changkun.de>
2022-05-18 22:09:48 +0200

    all: move GPU related code to external packages

--- internal/gpu/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 Changkun Ou <changkun.de>. All rights reserved.
-// Use of this source code is governed by a GPLv3 license that
-// can be found in the LICENSE file.
-
-// Package gpu provides functions that runs on GPU.
-package gpu
